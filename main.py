38d45cc6d01d97669337fb1bc114aebad6106c4d
KevinED11 <mendez_kael@hotmail.com>
2023-09-23 12:02:54 -0700

    2

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from typing import Iterator
+"""
+los iteradores son un objeto que se puede iterar o recorrer y permiten un ahorro de memoria
+ya que sacan los valores de uno en uno a medida que se necesitan
+"""
+from typing import Iterator, Generator
 
+
+NAMES = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Fred",
+    "Ginny",
+    "Harriet",
+    "Ileana",
+    "Joseph",
+]
+CITIES = [
+    "Schenectady",
+    "Albany",
+]
 
 def gen_squares(n: int) -> Iterator[int]:
-    i = 0
+    i = 1
     while i <= n:
-        yield i ** 2
+        yield i**2
         i += 1
 
 
@@ -12,6 +33,26 @@ def main() -> None:
     for i in gen_squares(10):
         print(i)
 
+    print(next(gen_squares(10)))
+    print(type(gen_squares(10)))
 
-if __name__ == '__main__':
+    iterator_names: Iterator[str] = iter(NAMES)
+    try:
+        for _ in iterator_names:
+            print(next(iterator_names))
+    except StopIteration as err:
+        print(err)
+
+    print(type(iterator_names))
+
+    cities_iterator: Iterator[str] = iter(CITIES)
+    try:
+        print(next(cities_iterator))
+        print(next(cities_iterator))
+        print(next(cities_iterator))
+    except StopIteration as err:
+        print(err)
+
+
+if __name__ == "__main__":
     main()
